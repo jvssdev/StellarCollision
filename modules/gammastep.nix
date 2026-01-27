79dc02f0ed89e6d7b047accdcaccef8404751b27
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2026-01-27 19:18:53 -0300

    29

--- modules/gammastep.nix
+++ modules/gammastep.nix
@@ -17,37 +17,38 @@ in
     enable = mkOption {
       type = types.bool;
       default = false;
-      description = "Enable gammastep as a user service with tray.";
+      description = "Enable gammastep with tray and manual provider.";
     };
+
     package = mkOption {
       type = types.package;
       default = pkgs.gammastep;
-      description = "The gammastep package to use.";
     };
-    temperatureDay = mkOption {
-      type = types.int;
-      default = 5500;
-      description = "Daytime color temperature.";
+
+    temperature = {
+      day = mkOption {
+        type = types.int;
+        default = 5500;
+      };
+      night = mkOption {
+        type = types.int;
+        default = 3500;
+      };
     };
-    temperatureNight = mkOption {
-      type = types.int;
-      default = 3500;
-      description = "Nighttime color temperature.";
-    };
+
     dawnTime = mkOption {
       type = types.str;
       default = "6:00-7:45";
-      description = "Dawn transition time (manual mode).";
     };
+
     duskTime = mkOption {
       type = types.str;
       default = "18:35-20:45";
-      description = "Dusk transition time (manual mode).";
     };
+
     tray = mkOption {
       type = types.bool;
       default = true;
-      description = "Show tray icon (uses gammastep-indicator).";
     };
   };
 
@@ -60,8 +61,8 @@ in
 
       xdg.config.files."gammastep/config.ini".text = ''
         [general]
-        temp-day=${toString cfg.temperatureDay}
-        temp-night=${toString cfg.temperatureNight}
+        temp-day=${toString cfg.temperature.day}
+        temp-night=${toString cfg.temperature.night}
         dawn-time=${cfg.dawnTime}
         dusk-time=${cfg.duskTime}
         fade=1
