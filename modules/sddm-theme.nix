93c0f0387047706826ab45c5dde826ad34b7cba9
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2026-01-27 15:38:53 -0300

    12

--- modules/sddm-theme.nix
+++ modules/sddm-theme.nix
@@ -18,7 +18,7 @@ let
 
   wallpaper = ../assests/Wallpapers/nord_valley.png;
 
-  background-derivation = pkgs.runCommand "sddm-bg.jpg" { } ''
+  background-derivation = pkgs.runCommand "bg.jpg" { } ''
     cp ${wallpaper} $out
   '';
 
@@ -48,10 +48,133 @@ let
         icon-size = 30;
         content-color = c.base05;
         active-content-color = c.base06;
-        background-color = c.base01;
+        background-color = c.base00;
+        background-opacity = 0.7;
+        active-background-color = c.base0D;
+        active-background-opacity = 0.7;
+        border-size = 2;
+        border-color = c.base0D;
+      };
+      "LoginScreen.LoginArea.PasswordInput" = {
+        width = 460;
+        height = 60;
+        font-size = 22;
+        display-icon = true;
+        icon-size = 30;
+        content-color = c.base05;
+        background-color = c.base00;
+        background-opacity = 0.7;
+        border-size = 2;
+        border-color = c.base0D;
+        margin-top = 20;
+      };
+      "LoginScreen.LoginArea.Spinner" = {
+        text = "Logging in";
+        font-size = 36;
+        icon-size = 72;
+        color = c.base06;
+        spacing = 1;
+      };
+      "LoginScreen.LoginArea.Username" = {
+        font-size = 40;
+        color = c.base00;
+        margin = 5;
+      };
+      "LoginScreen.LoginArea.WarningMessage" = {
+        font-size = 22;
+        normal-color = c.base06;
+        warning-color = c.base0A;
+        error-color = c.base08;
+      };
+      "LoginScreen.MenuArea.Buttons" = {
+        size = 60;
       };
       "LoginScreen.MenuArea.Keyboard" = {
         display = true;
+      };
+      "LoginScreen.MenuArea.Layout" = {
+        index = 2;
+        position = "bottom-center";
+        font-size = 20;
+        icon-size = 32;
+        content-color = c.base05;
+        active-content-color = c.base06;
+        background-color = c.base00;
+        background-opacity = 0.7;
+        border-size = 2;
+        border-color = c.base0D;
+      };
+      "LoginScreen.MenuArea.Popups" = {
+        max-height = 600;
+        item-height = 60;
+        item-spacing = 1;
+        padding = 2;
+        font-size = 22;
+        icon-size = 24;
+        content-color = c.base05;
+        active-content-color = c.base06;
+        background-color = c.base00;
+        background-opacity = 0.7;
+        active-option-background-color = c.base02;
+        active-option-background-opacity = 0.7;
+        border-size = 2;
+        border-color = c.base0D;
+        display-scrollbar = true;
+      };
+      "LoginScreen.MenuArea.Power" = {
+        index = 0;
+        popup-width = 200;
+        position = "bottom-center";
+        icon-size = 32;
+        content-color = c.base05;
+        active-content-color = c.base06;
+        background-color = c.base00;
+        background-opacity = 0.7;
+        border-size = 2;
+        border-color = c.base0D;
+      };
+      "LoginScreen.MenuArea.Session" = {
+        index = 1;
+        position = "bottom-center";
+        button-width = 300;
+        popup-width = 300;
+        font-size = 25;
+        icon-size = 32;
+        content-color = c.base05;
+        active-content-color = c.base06;
+        background-color = c.base00;
+        background-opacity = 0.7;
+        active-background-opacity = 0.7;
+        border-size = 2;
+        border-color = c.base0D;
+      };
+      LockScreen = {
+        background = "${background-derivation.name}";
+        blur = 50;
+      };
+      "LockScreen.Clock" = {
+        position = "center";
+        align = "center";
+        format = "hh:mm:ss";
+        color = c.base01;
+        font-size = 92;
+      };
+      "LockScreen.Date" = {
+        margin-top = 1;
+        format = "dd/MM/yyyy";
+        locale = "pt_BR";
+        color = c.base0D;
+        font-size = 32;
+      };
+      "LockScreen.Message" = {
+        text = "Press any key";
+        font-size = 32;
+        color = c.base0D;
+        icon-size = 44;
+        paint-icon = true;
+      };
+      Tooltips = {
+        enable = false;
       };
     };
   };
@@ -79,6 +202,12 @@ in
         qt6.qtwayland
       ];
 
+      etc."sddm.conf.d/cursor.conf".text = ''
+        [Theme]
+        CursorTheme=Bibata-Modern-Ice
+        CursorSize=24
+      '';
+
       sessionVariables = {
         XCURSOR_THEME = "Bibata-Modern-Ice";
         XCURSOR_SIZE = "24";
@@ -101,7 +230,8 @@ in
       extraPackages = silentTheme.propagatedBuildInputs;
       settings = {
         General = {
-          GreeterEnvironment = "QML2_IMPORT_PATH=${silentTheme}/share/sddm/themes/${silentTheme.pname}/components/,QT_QUICK_CONTROLS_STYLE=org.kde.desktop";
+          GreeterEnvironment = "QML2_IMPORT_PATH=${silentTheme}/share/sddm/themes/${silentTheme.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard,XCURSOR_THEME=Bibata-Modern-Ice,XCURSOR_SIZE=24,XCURSOR_PATH=/usr/share/icons:${pkgs.bibata-cursors}/share/icons";
+          InputMethod = "qtvirtualkeyboard";
         };
         Theme = {
           CursorTheme = "Bibata-Modern-Ice";
