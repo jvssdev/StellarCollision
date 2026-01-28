9260cce598cd9cd72f9e4c9121fe04de9df0eaa7
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2026-01-28 12:58:15 -0300

    yazi filechooser

--- modules/xdg-portal.nix
+++ modules/xdg-portal.nix
@@ -5,14 +5,20 @@
   ...
 }:
 let
-  inherit (lib) mkEnableOption mkIf;
-  cfg = config.cfg.portals;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    getExe
+    getExe'
+    ;
   homeDir = config.cfg.vars.homeDirectory;
+  cfg = config.cfg.portals;
 in
 {
   options.cfg.portals = {
     enable = mkEnableOption "XDG Desktop Portals configuration";
   };
+
   config = mkIf cfg.enable {
     xdg = {
       portal = {
@@ -40,33 +46,59 @@ in
         };
       };
     };
+
     hj.xdg.config.files."xdg-desktop-portal-termfilechooser/config" = {
       text = ''
         [filechooser]
-        cmd=${homeDir}/.config/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+        cmd=${getExe pkgs.bash} ${homeDir}/.config/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
         default_dir=${homeDir}
-        open_mode=suggested
-        save_mode=last
       '';
     };
+
     hj.xdg.config.files."xdg-desktop-portal-termfilechooser/yazi-wrapper.sh" = {
       executable = true;
       text = ''
-        #!${pkgs.bash}/bin/bash
-        set -euo pipefail
+        #!${getExe pkgs.bash}
+        set -e
 
-        multiple="$1"
-        directory="$2"
-        save="$3"
-        path="$4"
         out="$5"
+        path="$4"
 
-        ${pkgs.wezterm}/bin/wezterm start \
+        export PATH="${
+          lib.makeBinPath [
+            pkgs.wezterm
+            pkgs.yazi
+            pkgs.coreutils
+          ]
+        }:$PATH"
+
+        USER_ID=$(${getExe' pkgs.coreutils "id"} -u)
+        export USER=$(${getExe' pkgs.coreutils "id"} -un)
+        export HOME="${homeDir}"
+        export XDG_RUNTIME_DIR="/run/user/$USER_ID"
+
+        export DISPLAY=":11"
+        unset WAYLAND_DISPLAY
+
+        export XMODIFIERS="@im=fcitx"
+        export GTK_IM_MODULE="fcitx"
+        export QT_IM_MODULE="fcitx"
+        export SDL_IM_MODULE="fcitx"
+        export GLFW_IM_MODULE="ibus"
+
+        export XKB_CONFIG_ROOT="${pkgs.xkeyboard_config}/share/X11/xkb"
+        export XKB_DEFAULT_LAYOUT="br"
+
+        if [ ! -d "$path" ]; then
+          path="$HOME"
+        fi
+
+        exec ${getExe pkgs.wezterm} start \
           --always-new-process \
           --class filechooser \
-          -- ${pkgs.yazi}/bin/yazi \
+          -- ${getExe pkgs.yazi} \
           --chooser-file="$out" \
-          "$path"
+          "$path" >> /tmp/portal-debug.log 2>&1
       '';
     };
   };
