55124552d017cb0827e699a641f2c2911f6c7fc7
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2026-02-04 20:31:14 -0300

    fuzzel

--- modules/fuzzel.nix
+++ modules/fuzzel.nix
@@ -9,11 +9,11 @@ let
     mkOption
     types
     mkIf
+    strip
     ;
   cfg = config.cfg.fuzzel;
   c = config.cfg.theme.colors;
   inherit (config.cfg.fonts.monospace) name;
-
 in
 {
   options.cfg.fuzzel = {
@@ -22,14 +22,12 @@ in
       default = false;
       description = "Enable Fuzzel configuration.";
     };
-
     package = mkOption {
       type = types.package;
       default = pkgs.fuzzel;
       description = "The Fuzzel package to install.";
     };
   };
-
   config = mkIf cfg.enable {
     hj = {
       packages = [
@@ -38,29 +36,31 @@ in
       xdg.config = {
         files = {
           "fuzzel/fuzzel.ini".text = ''
+            [main]
+            dpi-aware=no
+            font=${name}:size=13
+            icons-enabled=yes
+            icon-theme=${config.cfg.gtk.iconTheme.name}
+            lines=15
+            prompt="󰍉  "
+            monospace=${name}
+            width=40
+            terminal=${config.cfg.vars.terminal}
+
             [border]
             radius=10
             width=2
 
             [colors]
-            background="${c.base00}f0"
-            border="${c.base05}ff"
-            selection="${c.base0D}ff"
-            selection-text="${c.base00}ff"
-            text="${c.base05}ff"
+            background=${strip c.base00}f0
+            border=${strip c.base0D}ff
+            selection=${strip c.base0D}ff
+            selection-text=${strip c.base00}ff
+            text=${strip c.base05}ff
 
             [dmenu]
             lines=25
             width=70
-
-            [main]
-            dpi-aware=no
-            font="${name}:size=13"
-            icon-theme="${config.cfg.gtk.iconTheme.name}"
-            lines=15
-            prompt="󰍉  "
-            monospace="${name}"
-            width=40
           '';
         };
       };
