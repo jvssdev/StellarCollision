2b4f6842f75bff3780a85b252aa60e6ff46fb6de
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2026-02-05 23:12:17 -0300

    colors 2

--- modules/themes/colors.nix
+++ modules/themes/colors.nix
@@ -21,19 +21,19 @@ in
     colors = {
       base00 = mkColor "#000000";
       base01 = mkColor "#04080F";
-      base02 = mkColor "#485A82";
-      base03 = mkColor "#6578B2";
+      base02 = mkColor "#2D3D60";
+      base03 = mkColor "#485A82";
       base04 = mkColor "#C3DDF9";
-      base05 = mkColor "#63789D";
-      base06 = mkColor "#284189";
-      base07 = mkColor "#5A5293";
+      base05 = mkColor "#485A82";
+      base06 = mkColor "#E0DEF4";
+      base07 = mkColor "#A7AAE7";
 
       base08 = mkColor "#CC1512";
       base09 = mkColor "#F29A8A";
       base0A = mkColor "#FCE570";
       base0B = mkColor "#0EA2AB";
       base0C = mkColor "#405D73";
-      base0D = mkColor "#2B6BBA";
+      base0D = mkColor "#344EA2";
       base0E = mkColor "#6B59C0";
       base0F = mkColor "#6699CC";
     };
