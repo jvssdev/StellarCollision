05e18cd044673e13a1b1dfc82640b664b2e80ff2
João Víctor Santos Silva <joao.victor.ss.dev@gmail.com>
2026-02-17 19:07:00 -0300

    niri flake cache

--- modules/shared/common.nix
+++ modules/shared/common.nix
@@ -162,11 +162,13 @@
         "https://nix-community.cachix.org"
         "https://cache.garnix.io"
         "https://wezterm.cachix.org"
+        "https://niri.cachix.org"
       ];
       extra-trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "wezterm.cachix.org-1:kAbhjYUC9qvblTE+s7S+kl5XM1zVa4skO+E/1IDWdH0="
+        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
       ];
     };
   };
